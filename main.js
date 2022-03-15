616a5d2181bf2928b6c33b9e8b825c97155f9d20
Joanna <joannaperdomo27@gmail.com>
2022-03-15 00:42:08 -0600

    cambios en css

--- main.js
+++ main.js
@@ -25,12 +25,12 @@ const cursos = [
 
 // Variables usuario
 
-let nombre = prompt('¿Cuál es tu nombre?');
+/* let nombre = prompt('¿Cuál es tu nombre?');
 let apellido = prompt('¿Cuál es tu apellido?');
 let edad = prompt('¿Cuál es tu edad?');
 let nivel = parseInt(prompt("¿Qué nivel vas a cursar? Escribe \n 1 para Principante \n 2 para Intermedio \n 3 para avanzados")); 
 let modalidad = prompt("¿Cuál horario quieres cursar? Escribe \n 1 para Semanales \n 2 para intensivos \n 3 para sabatinos");
-
+ */
 // Array con alumnos inscritos
 
 const todosLosAlumnos = [];
