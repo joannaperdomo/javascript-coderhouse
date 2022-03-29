539281e736456c457c0d768ea2aaa1f34bbda088
Joanna <joannaperdomo27@gmail.com>
2022-03-29 00:46:53 -0600

    añadiendo quiz

--- js/quizz.js
+++ js/quizz.js
@@ -1,65 +1,166 @@
 
 
 // Funciones para el quiz
+
+let contadordeRespuestas = 0;
+
 function crearQuiz(){
     // variable para guardar preguntas
-    const output = [];
-
-    // para cada pregunta
-    preguntas.forEach((preguntaActual, numeroPregunta) => {
-        const respuestas = [];
-
-        for(opcion in preguntaActual.answers){
-            respuestas.push(
-                `${opcion}: ${preguntaActual.answers[opcion]}`
-            );
-        };
-
-        output.push(
-            console.log(
-                `${preguntaActual.question} \n
-                ${respuestas.join(" ")}
-                `)
-            )
-        }
-    )
+    const opciones = [];
+    for (opcion in preguntas[contadordeRespuestas].answers){
+        opciones.push(
+            `<label>
+            <input type="radio" name="question" value="${opcion}">
+            ${preguntas[contadordeRespuestas].answers[opcion]}
+            </label><br>`
+        )
+    }
+    resultadoHTML = opciones.join(" ");
+    quizContenedor.innerHTML = `
+    <h2>${preguntas[contadordeRespuestas].question}</h2><br>
+    ${resultadoHTML}`;
 };
-
-function mostrarResultados(){};
 
 // Get elements
 const quizContenedor = document.getElementById("quiz");
-const resultadosContenedor = document.getElementById("resultados");
 const botonEnviar = document.getElementById("enviar");
+
 
 // Array con preguntas y respuestas
 const preguntas = [
     {
-        question: "Test",
+        question: "はじめまして、わたし　の　なまえ　________  モニカ　です。",
         answers: {
-            1: "uno",
-            2: "dos",
-            3: "tres"
+            1: "が",
+            2: "を",
+            3: "は"
         },
-        respuesta: 2
+        respuesta: 3
     },
     {
-        question: "Test2",
+        question: "日__語 は　おもしろい　です。",
         answers: {
-            1: "uno",
-            2: "dos",
-            3: "tres"
+            1: "本",
+            2: "体",
+            3: "休"
+        },
+        respuesta: 1
+    },
+    {
+        question: "日__語 は　おもしろい　です。",
+        answers: {
+            1: "本",
+            2: "体",
+            3: "休"
+        },
+        respuesta: 1
+    },
+    {
+        question: "日__語 は　おもしろい　です。",
+        answers: {
+            1: "本",
+            2: "体",
+            3: "休"
+        },
+        respuesta: 1
+    },
+    {
+        question: "日__語 は　おもしろい　です。",
+        answers: {
+            1: "本",
+            2: "体",
+            3: "休"
+        },
+        respuesta: 1
+    },
+    {
+        question: "日__語 は　おもしろい　です。",
+        answers: {
+            1: "本",
+            2: "体",
+            3: "休"
+        },
+        respuesta: 1
+    },
+    {
+        question: "日__語 は　おもしろい　です。",
+        answers: {
+            1: "本",
+            2: "体",
+            3: "休"
+        },
+        respuesta: 1
+    },
+    {
+        question: "日__語 は　おもしろい　です。",
+        answers: {
+            1: "本",
+            2: "体",
+            3: "休"
+        },
+        respuesta: 1
+    },
+    {
+        question: "日__語 は　おもしろい　です。",
+        answers: {
+            1: "本",
+            2: "体",
+            3: "休"
+        },
+        respuesta: 1
+    },
+    {
+        question: "日__語 は　おもしろい　です。",
+        answers: {
+            1: "本",
+            2: "体",
+            3: "休"
         },
         respuesta: 1
     }
 ];
 
 // Mostrar el quiz
+
 crearQuiz();
 
-// Mostrar resultados al presionar boton
-botonEnviar.onclick(mostrarResultados());
+// Añadir respuestas y pasar a la siguiente pregunta
 
+const respuestasDelUsuario = [];
+
+botonEnviar.onclick = () => {
+    const elemento = document.getElementsByName("question");
+    for (i = 0; i < elemento.length; i++){
+        if(elemento[i].checked){
+            const obj = {pregunta:preguntas[contadordeRespuestas].question,respuesta:elemento[i].value}
+            respuestasDelUsuario.push(obj);
+        }
+    };
+    if (contadordeRespuestas < preguntas.length - 1){
+        contadordeRespuestas++
+        crearQuiz()
+    } else {
+        botonEnviar.remove();
+        mostrarResultados()
+    }
+};
+
+// Funcion para mostrar los resultados
+
+let respuestasAcertadas = [];
+
+function mostrarResultados(){
+
+    respuestasDelUsuario.forEach(respuesta => {
+       let preguntaBase = preguntas.find((el) => el.question == respuesta.pregunta);
+        if (preguntaBase.respuesta == respuesta.respuesta){
+            respuestasAcertadas.push("prueba");
+        }
+    });
+
+    quizContenedor.innerText = `Has acertado ${respuestasAcertadas.length} de ${preguntas.length}`
+
+};
 
 
 
