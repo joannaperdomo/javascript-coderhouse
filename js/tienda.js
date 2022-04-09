ccc1aef3135c072f0b3f9f79f6b964fc18b2376d
Joanna <joannaperdomo27@gmail.com>
2022-04-09 02:48:12 -0500

    diseñando la tienda

---
+++ js/tienda.js
@@ -0,0 +1,45 @@
+// Constructor de producto
+function Producto(nombre,codigo,precio,stock,nivel){
+    this.nombre = nombre;
+    this.codigo = codigo;
+    this.precio = precio;
+    this.stock = stock;
+    this.img = `${codigo}.png`;
+    this.nivel = nivel
+}
+// Array con los productos
+const productos = [
+    producto = new Producto ("Dekiru Nihongo Rojo", "dekiru-nihongo-principiante",300,23,"Principiante"),
+    producto = new Producto ("Dekiru Nihongo Amarillo", "dekiru-nihongo-intermedio",400,20,"Intermedio"),
+    producto = new Producto ("Dekiru Nihongo Azul", "dekiru-nihongo-avanzado",500,5,"Avanzado"),
+    producto = new Producto ("Shinkansen Master N3 - Lectura", "shinkansen-3-reading",530,6,"Avanzado"),
+    producto = new Producto ("Shinkansen Master N4 - Lectura", "shinkansen-4-reading",350,10,"Intermedio"),
+    producto = new Producto ("Shinkansen Master N4 - Vocabulario", "shinkansen-4-vocabulario",400,7,"Intermedio")
+];
+
+// Loop para generar las cards de cada producto
+
+for (let i = 0; i < productos.length; i++){
+    let containerProductos = document.getElementById('productos');
+    let producto = document.createElement("div");
+    producto.innerHTML = `
+    <div class="texto-producto">
+    <p>${productos[i].nombre}<p>
+    <p>$${productos[i].precio}</p>
+    <a class="btn btn-dark añadir-al-carrito-btn"" >Añadir</a></div>
+    <img src="../img/productos/${productos[i].img}" alt="${productos[i].nombre}">`;
+    producto.className = "producto";
+    containerProductos.append(producto);  
+};
+
+// Get elements del DOM
+let botonDeAñadirAlCarrito = document.getElementsByClassName('añadir-al-carrito-btn');
+
+botonDeAñadirAlCarrito[0].onclick = (e) => console.log(e.target);
+
+function test () {
+    console.log('holi')
+}
+
+
+
